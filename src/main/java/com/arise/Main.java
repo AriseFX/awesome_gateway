bce3f9908da380b10e28b8806e7a9a28d4ff20bc
Arise <18856166078@163.com>
2021-03-22 20:19:30 +0800

    develop提交3

--- src/main/java/com/arise/Main.java
+++ src/main/java/com/arise/Main.java
@@ -13,7 +13,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  */
 @SpringBootApplication
 public class Main {
-
     public static void main(String[] args) {
         new SpringApplicationBuilder(Main.class)
                 .bannerMode(Banner.Mode.OFF)
