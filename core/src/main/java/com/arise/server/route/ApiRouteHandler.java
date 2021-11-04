038beda9bf07f6a3769c17a3b8a74ba5fc14ea1a
arise <18856166078@qq.com>
2021-11-04 17:05:26 +0800

    获取host提高性能

--- core/src/main/java/com/arise/server/route/ApiRouteHandler.java
+++ core/src/main/java/com/arise/server/route/ApiRouteHandler.java
@@ -115,7 +115,7 @@ public class ApiRouteHandler extends ChannelInboundHandlerAdapter {
                                 DefaultChannelPipeline pipeline = (DefaultChannelPipeline) outbound.pipeline();
                                 HttpHeaders headers = request.headers();
                                 headers.set(HttpHeaderNames.CONNECTION, "keep-alive");
-                                headers.set(HttpHeaderNames.HOST, inetAddress.getHostName());
+                                headers.set(HttpHeaderNames.HOST, inetAddress.getHostString());
                                 pipeline.addLast(new ForwardHandler(respPromise, inbound));
                                 new FilterContext(contents
                                         , respPromise, forwardFilters, eventLoop, attr, null).handleNext();
